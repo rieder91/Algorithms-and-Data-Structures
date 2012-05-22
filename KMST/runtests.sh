dd0c7f78b575fba36f4407ff284d5ca49b0072a7
Thomas Rieder <tho.rieder@aon.at>
2012-05-23 01:39:09 +0200

    added script for automated testing
    uses unix paths

---
+++ KMST/runtests.sh
@@ -0,0 +1,10 @@
+echo "Test Case #1"
+java ads1ss12.pa.Main tests/input/0000
+echo "Test Case #2"
+java ads1ss12.pa.Main tests/input/0001
+echo "Test Case #3"
+java ads1ss12.pa.Main tests/input/0002
+echo "Test Case #4"
+java ads1ss12.pa.Main tests/input/0003
+echo "Test Case #5"
+java ads1ss12.pa.Main tests/input/0004
