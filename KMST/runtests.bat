748aee1a212b69b779e27d19e2eed9fbac9bf4b0
Thomas Rieder <tho.rieder@aon.at>
2012-05-23 17:52:22 +0200

    test script for windows

---
+++ KMST/runtests.bat
@@ -0,0 +1,5 @@
+java ads1ss12.pa.Main tests/input/0000
+java ads1ss12.pa.Main tests/input/0001
+java ads1ss12.pa.Main tests/input/0002
+java ads1ss12.pa.Main tests/input/0003
+java ads1ss12.pa.Main tests/input/0004
